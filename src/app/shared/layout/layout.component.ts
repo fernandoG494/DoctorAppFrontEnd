96262e1183baf08bb7b6e037a0e2047bc555308a
Fernando García (Incuerd0) <42920098+fernandoG494@users.noreply.github.com>
2024-07-03 22:47:27 -0600

    iii

--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+import { SharedService } from '../shared.service';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
-export class LayoutComponent {}
+export class LayoutComponent implements OnInit {
+  username: string = '';
+
+  constructor(private router: Router, private sharedService: SharedService) {}
+
+  ngOnInit(): void {
+    const userToken = this.sharedService.getSession();
+
+    if (userToken !== null) {
+      this.username = userToken.userName;
+    }
+  }
+
+  closeSession() {
+    this.sharedService.deleteSession();
+    this.router.navigate(['login']);
+  }
+}
